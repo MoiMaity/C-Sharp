fbff66a20e3693ff0b283097cbfffba2af65f319
Moitrish Maity <69980373+MoiMaity@users.noreply.github.com>
2024-11-05 10:26:39 +0530

    Add files via upload

---
+++ Mini Projects/WordCount/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string sec = Console.ReadLine();
+        int wordcount = await CountWords(sec);
+        Console.WriteLine($"The number of words in the sentence is : {wordcount}");
+    }
+
+    static async Task<int> CountWords(string s)
+    {
+        await Task.Delay(1);
+        int wc = s.Split(new[]{ ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        return wc;
+    }
+}
